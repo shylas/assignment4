4b9e6b04426e9ce82050a7ee181e5ff66ce63359
Sam Hylas <shylas@uw.edu>
2017-05-11 17:46:13 -0700

    adding scripts api key

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ function updateMap() {
   var latitude = $("#latitude").val();
   var longitude= $("#longitude").val();
   var zoom= $("#zoom-level").val();
-  var newsrc = "https://maps.googleapis.com/maps/api/staticmap?size=600x400&center="+latitude+","+longitude+"&zoom="+zoom+"&markers=color:red|"+latitude+","+longitude+"&key=AIzaSyDU6lWXAex1uw3neucrHf9BSGRExTkwqxQ";
+  var newsrc = "https://maps.googleapis.com/maps/api/staticmap?size=600x400&center="+latitude+","+longitude+"&zoom="+zoom+"&markers=color:red|"+latitude+","+longitude+"&key=AIzaSyBBPAu8y4ITwSFsaHUPY2Ap4k3zE_oQ2mY";
   console.log(newsrc);
   
   $('#static-map').attr("src",newsrc);
